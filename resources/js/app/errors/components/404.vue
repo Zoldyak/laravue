78d175b96673f5439b034f484a30a6038fbcf5bd
Zoldyak <firman.akun1@gmail.com>
2023-11-04 16:19:28 +0700

    membuat halaman login

---
+++ resources/js/app/errors/components/404.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>
+        <center>
+            404 Halaman Tidak ditemukan
+        </center>
+        
+    </h1>
+</template>
---
+++ resources/js/app/errors/router/index.js
@@ -0,0 +1,10 @@
+import {NotFound} from '../components';
+
+
+export default [
+    {
+    path:"*",
+    component: NotFound,
+    }
+];
+
